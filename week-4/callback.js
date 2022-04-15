a4c8f7dd8a04fb2a647a22409dac488d33c2cb72
Grace_Chen <grace.chen.610@gmail.com>
2022-04-16 06:06:47 +0800

    Week-4作業1、2

--- week-4/callback.js
+++ week-4/callback.js
@@ -1,8 +1,8 @@
-//Assignment 1: 
+//Assignment 1: Callback Function
 function delayedResult(n1, n2, delayTime, callback){
     // your code here
     window.setTimeout( 
-        (result) => {
+        function(){
             result = n1 + n2 ;
             console.log(result);
             window.alert(result);
@@ -20,20 +20,37 @@ delayedResult(-5, 10, 2000, function(result){
 }); // 5 (-5+10) will be shown in an alert dialog after 2 seconds
 
 
-//Assignment 2: Callback Function and Advanced HTML DOM
 
+//Assignment 2: Callback Function and Advanced HTML DOM
 function ajax(src, callback){
     // your code here
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4){
+            const jsonToStr = JSON.parse(xhr.responseText);
+            callback = render(jsonToStr);
+        }
     }
-    function render(data){
-    // your code here.
-    // document.createElement() and appendChild() are preferred. 
-    //No innerHTML or something alike
+    xhr.open("GET",src);
+    xhr.send();
+};
+
+function render(jsonToStr){
+// your code here. // document.createElement() and appendChild() are preferred. // No innerHTML or something alike
+    for(let i = 0; i<jsonToStr.length; i++){
+        let name = jsonToStr[i].name;
+        let price = jsonToStr[i].price;
+        let description = jsonToStr[i].description;
+        const tr = document.querySelectorAll("tr")[i];
+        const td = document.createElement("td");
+        let text = document.createTextNode(`name: ${name}, price: ${price}, description: ${description}`);
+        tr.appendChild(td).appendChild(text);
     }
-    ajax(
-    "https://appworks-school.github.io/Remote-Aassigiment-Data/products",
-    function(response){
-        render(response);
-    }
-); // you should get product information in JSON format and render data in the page
+}
+
+ajax(
+"https://appworks-school.github.io/Remote-Aassigiment-Data/products",
+function(response){
+    render(response);
+}); // you should get product information in JSON format and render data in the page
     
